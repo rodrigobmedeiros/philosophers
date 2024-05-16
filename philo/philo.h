1a322dd04a96483914aaa144aafb482ef86aea41
root <root@BellaGabiDev>
2024-05-15 23:40:38 -0300

    Create a base environment to develop the project

---
+++ philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robernar <robernar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 23:08:08 by robernar          #+#    #+#             */
+/*   Updated: 2024/02/24 19:11:10 by robernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+
+#endif
